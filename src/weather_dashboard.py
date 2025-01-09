e17856c7a08820093883f0633d4c58ffc0ded05a
maxter97 <shawnjr13@gmail.com>
2025-01-08 21:36:44 -0500

    update code

--- src/weather_dashboard.py
+++ src/weather_dashboard.py
@@ -13,12 +13,7 @@ load_dotenv()
 class WeatherDashboard:
     def __init__(self):
         self.api_key = os.getenv('OPENWEATHER_API_KEY')
-        
-        # Generate a unique bucket name
-        random_number = random.randint(1000, 9999)
-        self.bucket_name = f"weather-dashboard-{random_number}"
-        os.environ['AWS_BUCKET_NAME'] = self.bucket_name
-        
+        self.bucket_name = os.getenv('AWS_BUCKET_NAME')
         profile_name = os.getenv('AWS_PROFILE', 'default')
         
         try:
@@ -32,13 +27,33 @@ class WeatherDashboard:
 
     def create_bucket_if_not_exists(self):
         """Create S3 bucket if it doesn't exist"""
+        if self.bucket_name:
+            try:
+                self.s3_client.head_bucket(Bucket=self.bucket_name)
+                print(f"Bucket {self.bucket_name} exists")
+                return
+            except botocore.exceptions.ClientError as e:
+                error_code = int(e.response['Error']['Code'])
+                if error_code == 404:
+                    print(f"Bucket {self.bucket_name} does not exist in AWS")
+                    try:
+                        self.s3_client.create_bucket(Bucket=self.bucket_name)
+                        print(f"Successfully created bucket {self.bucket_name}")
+                        return
+                    except Exception as e:
+                        print(f"Error creating bucket: {e}")
+                        return
+                else:
+                    print(f"Error checking bucket: {e}")
+                    return
+        else:
+            print("Bucket name not found in .env file")
+
         try:
-            self.s3_client.head_bucket(Bucket=self.bucket_name)
-            print(f"Bucket {self.bucket_name} exists")
-        except:
-            print(f"Creating bucket {self.bucket_name}")
-        try:
-            # Simpler creation for us-east-1
+            # Generate a unique bucket name
+            random_number = random.randint(1000, 9999)
+            self.bucket_name = f"weather-dashboard-{random_number}"
+            os.environ['AWS_BUCKET_NAME'] = self.bucket_name
             self.s3_client.create_bucket(Bucket=self.bucket_name)
             print(f"Successfully created bucket {self.bucket_name}")
         except Exception as e:
